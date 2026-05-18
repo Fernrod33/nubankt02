a8d912bd40c60fd6309b5912a1bee6358250b901
patrickissl <patrickissler1234@gmail.com>
2026-05-18 19:37:22 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nubankt02/pages/secundaria.dart';
+import 'package:nubankt02/pages/myHomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: GoogleFonts.poppins().fontFamily,
+
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/secundaria',
+      routes: {
+        '/secundaria': (context) => const Secundaria(),
+        '/primaria': (context) => const MyHomePage()
+      },
+
+    );
+  }
+}
+
